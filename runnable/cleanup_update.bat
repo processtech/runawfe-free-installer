ee8a8a53f5771710b9188318512cdded2f4cd5b7
Andrey Pletenev <pletenev.andrey@yandex.ru>
2025-12-26 14:50:15 +0700

    rm4359 Add cleanup for update when gpd not selected

--- runnable/cleanup_update.bat
+++ runnable/cleanup_update.bat
@@ -1,4 +1,8 @@
 if "$delete.gpd" == "true" (
     rmdir /s /q "$INSTALL_PATH\$gpd.subpath"
-    rmdir /s /q "$appdata\$APP_NAME"
+    rmdir /s /q "$appdata\$APP_NAME\$gpd.subpath"
+)
+if "$delete.rtn" == "true" (
+    rmdir /s /q "$INSTALL_PATH\$rtn.subpath"
+    del "$appdata\$APP_NAME\rtn.log"
 )
