96d5d4b4ce714a700ee5e67840f07aa70a1ab2ca
Andrey Pletenev <pletenev.andrey@yandex.ru>
2025-12-27 13:12:07 +0700

    rm4359 Add cleanup for notifier config file

--- runnable/cleanup_update.bat
+++ runnable/cleanup_update.bat
@@ -5,4 +5,5 @@ if "$delete.gpd" == "true" (
 if "$delete.rtn" == "true" (
     rmdir /s /q "$INSTALL_PATH\$rtn.subpath"
     del "$appdata\$APP_NAME\rtn.log"
+    del "%USERPROFILE%\config.properties"
 )
