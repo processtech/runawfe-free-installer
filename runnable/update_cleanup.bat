77444f446908a28fe561641596ff1bfd0b56eb1d
Andrey Pletenev <pletenev.andrey@yandex.ru>
2025-12-29 17:03:24 +0700

    rm4359  Add cleanup for update when simulator not selected

--- runnable/update_cleanup.bat
+++ runnable/update_cleanup.bat
@@ -7,4 +7,9 @@ if "$delete.rtn" == "true" (
   del "$appdata\$APP_NAME\rtn.log"
   del "%USERPROFILE%\config.properties"
 )
+if "$delete.simulator" == "true" (
+  rmdir /s /q "$INSTALL_PATH\$simulator.subpath"
+  rmdir /s /q "$appdata\$APP_NAME\jboss"
+  rmdir /s /q "%TEMP%\$APP_NAME"
+)
 exit /b 0
