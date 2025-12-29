44ed727fdac98f031682c3153da74ac94d0d7ac8
Andrey Pletenev <pletenev.andrey@yandex.ru>
2025-12-29 16:19:29 +0700

    rm4359 Add cleanup of temp folder when simulator installed

--- runnable/delete_activemq_cache.bat
+++
@@ -1,5 +0,0 @@
-set "ACTIVEMQ_CACHE_DIR=%APPDATA%\$APP_NAME\jboss\data\activemq"
-
-if exist "%ACTIVEMQ_CACHE_DIR%" (
-    rmdir /s /q "%ACTIVEMQ_CACHE_DIR%"
-)
---
+++ runnable/simulator_install_cleanup.bat
@@ -0,0 +1,2 @@
+rmdir /s /q "%APPDATA%\$APP_NAME\jboss\data\activemq"
+rmdir /s /q "%TEMP%\$APP_NAME"
