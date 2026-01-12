3cc7fd3d84127103958fcea5389c2c528e50f2c1
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-01-12 16:55:29 +0700

    rm4359 Stopping of server service during uninstall

---
+++ runnable/remove_server_service.bat
@@ -0,0 +1,24 @@
+@echo off
+chcp 65001 >nul
+
+setlocal
+
+set "NOPAUSE=1"
+set "LOGFILE=%TEMP%\runawfe_uninstall.log"
+
+msg * /TIME:20 "Выполняется остановка службы сервера RunaWFE. Это может занять порядка минуты. После этого деинсталляция продолжится автоматически."
+
+:: Очистка лога
+echo. > "%LOGFILE%" 2>nul
+
+echo [1/4] Остановка сервера WildFly... >> "%LOGFILE%"
+call "$INSTALL_PATH\$server.subpath\bin\jboss-cli.bat" --connect --controller=localhost:$jboss.management.http.port --command=:shutdown >> "%LOGFILE%" 2>&1
+
+echo [2/4] Завершение процесса jbosssvc.exe... >> "%LOGFILE%"
+taskkill /F /IM jbosssvc.exe >> "%LOGFILE%" 2>&1
+
+echo [3/4] Удаление службы >> "%LOGFILE%"
+sc delete $jboss.servicename >> "%LOGFILE%" 2>&1
+
+echo [4/4] Поиск и завершение java.exe, связанного с сервером >> "%LOGFILE%"
+taskkill /F /IM java.exe /FI "MODULES eq jboss-modules.jar" >> "%LOGFILE%" 2>&1
--- setenv.bat
+++ setenv.bat
@@ -4,3 +4,4 @@ set COMPILER_PATH="C:\Program Files\IzPack\bin\compile.bat"
 set XML_FILE=RunaWFE_installer.xml
 set IZPACK_OPTS=-Dfile.encoding=UTF-8
 set BUILD_ROOT=C:\runawfe-professional-autobuild\build\source\projects\installer\windows\target\NSIS7\src
+set SVCNAME=JBAS50SVC
