2db9b2ec3261045061309a6721cd04535c989aef
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-01-27 16:39:39 +0700

    rm4359 Add edition to install path

--- runnable/update_cleanup.bat
+++ runnable/update_cleanup.bat
@@ -1,15 +1,15 @@
 if "$delete.gpd" == "true" (
   rmdir /s /q "$INSTALL_PATH\$gpd.subpath"
-  rmdir /s /q "$appdata\$APP_NAME\$gpd.subpath"
+  rmdir /s /q "$appdata.install.path\$gpd.subpath"
 )
 if "$delete.rtn" == "true" (
   rmdir /s /q "$INSTALL_PATH\$rtn.subpath"
-  del "$appdata\$APP_NAME\rtn.log"
+  del "$appdata.install.path\rtn.log"
   del "%USERPROFILE%\config.properties"
 )
 if "$delete.simulator" == "true" (
   rmdir /s /q "$INSTALL_PATH\$simulator.subpath"
-  rmdir /s /q "$appdata\$APP_NAME\jboss"
+  rmdir /s /q "$appdata.install.path\jboss"
   rmdir /s /q "%TEMP%\$APP_NAME"
 )
 exit /b 0
