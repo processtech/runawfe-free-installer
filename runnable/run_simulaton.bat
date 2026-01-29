fa8c81ad99fad923ebeadd53c23fd5331b73f44e
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-01-29 15:40:39 +0700

    rm4359 minor: Reuse of Jboss log dir variable

--- runnable/run_simulaton.bat
+++ runnable/run_simulaton.bat
@@ -1,3 +1,3 @@
 @echo off
 SET JBOSS_LOG_DIR="%TEMP%\$APP_NAME\jboss\log"
-call standalone.bat "-Djboss.server.log.dir=%TEMP%\$APP_NAME\jboss\log" "-Djboss.server.temp.dir=%TEMP%\$APP_NAME\jboss\tmp" "-Djboss.server.base.dir=%APPDATA%\$APP_NAME\jboss"
+call standalone.bat "-Djboss.server.log.dir=%JBOSS_LOG_DIR%" "-Djboss.server.temp.dir=%TEMP%\$APP_NAME\jboss\tmp" "-Djboss.server.base.dir=%APPDATA%\$APP_NAME\jboss"
