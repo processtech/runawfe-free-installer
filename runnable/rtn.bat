58ae3744d09f43c8295dade003ce67c2299d2e73
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-02-03 13:08:39 +0700

    rm4359 Adapt task notifier to work with internal JRE

--- runnable/rtn.bat
+++ runnable/rtn.bat
@@ -1,5 +1,4 @@
 set DIRNAME=.\
 if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
 cd /D "%DIRNAME%"
-if "%JAVA_HOME%"=="" start javaw -Drtn.log.dir="$appdata.install.path" -jar rtn.jar
-if NOT "%JAVA_HOME%"=="" start "wnd" "%JAVA_HOME%\bin\javaw" -Drtn.log.dir="$appdata.install.path" -jar rtn.jar
+start "tn" "$java.path\bin\javaw.exe" -Drtn.log.dir="$appdata.install.path" -jar rtn.jar
