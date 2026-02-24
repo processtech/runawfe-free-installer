a3f0592d4a4954a32214c0db1ac08820105b6a6e
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-02-24 12:20:35 +0700

    rm4496 Change build root for free edition

--- setenv.bat
+++ setenv.bat
@@ -5,7 +5,7 @@ set INSTALLER_EXE=%WFE_APPNAME%-%WFE_EDITION%-%WFE_VERSION%.exe
 set COMPILER_PATH="C:\Program Files\IzPack\bin\compile.bat"
 set XML_FILE=RunaWFE_installer.xml
 set IZPACK_OPTS=-Dfile.encoding=UTF-8
-set BUILD_ROOT=C:\runawfe-professional-autobuild\build\source\projects\installer\windows\target\NSIS7\src
+set BUILD_ROOT=C:\runawfe-free-autobuild\build\source\projects\installer\windows\target\NSIS7\src
 set SVCNAME=JBAS50SVC
 set STATISTIC_REPORT_URL=https://usagereport.runawfe.org
 set STATISTIC_REPORT_DAYS_AFTER_ERROR=11
