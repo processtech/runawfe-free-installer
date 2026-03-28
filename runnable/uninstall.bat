dd6e9c33161b766a0af022387203c24504994582
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-03-28 12:42:11 +0700

    rm4496 Fix removing installation folder during deinstallation

--- runnable/uninstall.bat
+++ runnable/uninstall.bat
@@ -1,1 +1,4 @@
-"$java.executable" -jar "$INSTALL_PATH\Uninstaller\uninstaller.jar"
+@echo off
+set "TEMP_JAVA=%TEMP%\RunaWFE_uninstall_java_%RANDOM%"
+xcopy "$java.path" "%TEMP_JAVA%" /E /I /Y >nul
+"%TEMP_JAVA%\bin\javaw.exe" -jar "${INSTALL_PATH}\Uninstaller\uninstaller.jar"
