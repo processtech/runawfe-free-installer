6ba0f4a6199cf7ce7f5e44bff7e5da7927b1a016
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-06-08 13:29:06 +0700

    rm4496 Add edition to notifier configuration path

--- runnable/unix/rtn_start.sh
+++ runnable/unix/rtn_start.sh
@@ -4,6 +4,6 @@ if [ -f "../install_desktop_shortcut.sh" ]; then
     ../install_desktop_shortcut.sh "Task notifier"
     ../install_desktop_shortcut.sh "Server web interface"
 fi
-LOGDIR="\$HOME/.local/runawfe-notifier"
+LOGDIR="\$HOME/.local/@rtn.appname"
 mkdir -p "\$LOGDIR"
 "@java.executable" -Dorg.eclipse.swt.browser.UseWebKitGTK=true -Drtn.log.dir="\$LOGDIR" -cp "@INSTALL_PATH/@rtn.subpath/rtn.jar" ru.runa.notifier.PlatformLoader
