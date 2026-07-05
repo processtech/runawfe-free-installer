88a9b18df8d7ba2abbf8811c0ee14b3953ea2e1e
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-07-05 12:36:50 +0700

    rm4496  Use sudo for uninstaller

--- runnable/unix/uninstall.sh
+++ runnable/unix/uninstall.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 TEMP_JAVA="$(mktemp -d /tmp/RunaWFE_uninstall_java_XXXXXX)"
-trap 'RC=$?; rm -rf "$TEMP_JAVA"; exit $RC' EXIT INT TERM HUP
+
+trap 'rm -rf "$TEMP_JAVA"' EXIT INT TERM HUP
 
 cp -Rp "@java.path"/* "$TEMP_JAVA/"
-"$TEMP_JAVA/bin/java" -jar "@INSTALL_PATH/Uninstaller/uninstaller.jar"
+
+if [ "$(id -u)" -ne 0 ] && command -v sudo >/dev/null 2>&1; then
+    sudo -E "$TEMP_JAVA/bin/java" -jar "@INSTALL_PATH/Uninstaller/uninstaller.jar" &
+else
+    "$TEMP_JAVA/bin/java" -jar "@INSTALL_PATH/Uninstaller/uninstaller.jar" &
+fi
+
+MAIN_PID=$!
+
+wait $MAIN_PID 2>/dev/null
+
+while GUI_PID=$(pgrep -f "$TEMP_JAVA/bin/java"); do
+    wait $GUI_PID 2>/dev/null || sleep 1
+done
