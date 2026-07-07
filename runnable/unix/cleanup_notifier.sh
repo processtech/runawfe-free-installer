3ad1a1ac5bf241dec5bc20907d8a3f3a804c39c5
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-07-07 16:22:28 +0700

    rm4496 Cleanup rtn shortcuts from linux desktop and autostart

--- runnable/unix/cleanup_notifier.sh
+++ runnable/unix/cleanup_notifier.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-if [ -d "$rtn.properties.folder" ]; then
-    rm -rf "$rtn.properties.folder" 2>/dev/null || true
+if [ -d "@rtn.properties.folder" ]; then
+    rm -rf "@rtn.properties.folder" 2>/dev/null || true
 fi
+
+rm -f "/etc/xdg/autostart/@{rtn.shortcut.name}.desktop"
+
+if [ -f "@INSTALL_PATH/install_desktop_shortcut.sh" ]; then
+    "@INSTALL_PATH/install_desktop_shortcut.sh" "@rtn.shortcut.name" --delete
+fi
+
 exit 0
